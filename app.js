5512506e52682f5cb027baccb2022e59fe2c1a80
Malcolm Foster <malcolmfoster@gmail.com>
2018-04-16 14:43:45 -0600

    :construction:

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const reviewDbURL = "https://trekreviewsserver.herokuapp.com/comments"
 fetchSeriesData()
 fetchMovieData()
 fetchReviews()
-saveReview()
+// saveReview()
 
 document.getElementById("selectedMedia").addEventListener("change", populateSeriesInfo)
 
@@ -30,7 +30,7 @@ function fetchReviews() {
 
 function createSeriesDropdown(response) {
     let drop = document.getElementById("shows")
-    response.seasons.forEach(function(seasons) {
+    response.seasons.forEach(function (seasons) {
         let option = document.createElement("option")
         option.innerHTML = seasons.series.title + " Season " + seasons.seasonNumber
         drop.appendChild(option)
@@ -39,7 +39,7 @@ function createSeriesDropdown(response) {
 
 function createMoviesDropdown(movies) {
     let drop = document.getElementById("movies")
-    movies.movies.forEach(function(movies) {
+    movies.movies.forEach(function (movies) {
         let option = document.createElement("option")
         option.innerHTML = movies.title
         drop.appendChild(option)
@@ -48,7 +48,15 @@ function createMoviesDropdown(movies) {
 
 function populateReviews(reviews) {
     let reviewsDiv = document.getElementById("submittedReviews")
-}   reviews.comments
+    reviews.comments.forEach((reviews) => {
+        let h2 = document.createElement("h2")
+        h2.innerHTML = reviews.media
+        reviewsDiv.appendChild(h2)
+        let p = document.createElement("p")
+        p.innerHTML = reviews.comments
+        reviewsDiv.appendChild(p)
+    })
+}
 
 function populateSeriesInfo(series) {
     let form = document.querySelector("#seriesInfo")
@@ -63,34 +71,32 @@ function populateSeriesInfo(series) {
     submit.name = "save"
     submit.value = "Submit Review!"
     form.appendChild(submit)
+    form.addEventListener("submit", saveReview)
 }
 
-function saveReview() {
-    let submitButton = document.querySelector("button")
-    submitButton.addEventListener("click", (lgm) => {
-        lgm.preventDefault()
-        let formData = new FormData(document.querySelector("form"))
-        let data = {
-            medium: choice.value,
-            comments: formData.get("comments"),
-        }
-        fetch(reviewDbURL, {
-            method: "POST",
-            headers: new Headers({
-                "Content-Type": "application/json"
-            }),
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(response => {
-            showReview(response, data)
-        })
-        .catch(console.error);
+function saveReview(event) {
+    event.preventDefault()
+    let formData = new FormData(document.querySelector("form"))
+    let data = {
+        // media: formData.get(select.value)
+        media: select.value,
+        comments: formData.get("comments")
+    }
+    fetch(reviewDbURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
     })
+        .then(response => response.json())
+        // .then(response => {
+        //     showReview(response, data)
+        // })
+        .catch(console.error)
 }
 
-showReview(response, data) {
-    document.querySelector("#media").innerHTML = data.medium
-    document.querySelector("#review").innerHTML = data.comments
-}
-
+// function showReview(response, data) {
+//     document.querySelector("#media").innerHTML = data.media
+//     document.querySelector("#review").innerHTML = data.comments
+// }
